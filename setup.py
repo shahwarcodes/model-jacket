959236e09bcfc7ea4df4bd016ada23f70224bcc6
Shahwar Saleem <shahwars@mac.home>
2025-06-20 02:51:51 -0400

    Added new version with model_jacket name.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="model_jacket",
-    version="0.1.0",
+    version="0.2.0",
     description="Package any ML model into a scalable, observable FastAPI service",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
